6d0f0a6b80ab68757517e0978cc51d0aecb04acb
Sydney Prier <priersr@gmail.com>
2023-06-13 09:00:04 -0500

    Create InertTurn_robot.config.h

---
+++ InertTurn_robot.config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor LeftMotor;
+extern motor RightMotor;
+extern inertial Inertial20;
+
+// here we have to create the inertial sensor
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
